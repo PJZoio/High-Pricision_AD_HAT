f4673401a06bb6af40a0d0a1ee12aee0fe9d0b55
Bernardo Carvalho <bernardo.carvalho@tecnico.ulisboa.pt>
2024-01-06 19:46:38 +0000

    Added scripts
    Signed-off-by: Bernardo Carvalho <bernardo.carvalho@tecnico.ulisboa.pt>

---
+++ python/RP2040Serial.py
@@ -0,0 +1,45 @@
+"""
+A library to interface RP2024 microcontroller through serial connection
+to drive stepper motors
+https://pyserial.readthedocs.io/en/latest/shortintro.html
+
+"""
+import serial
+
+class RP2040():
+    """
+    Models an Arduino Serial connection
+    """
+
+    def __init__(self, serial_port='/dev/ttyACM0', baud_rate=230400,
+            read_timeout=20):
+        """
+        Initializes the serial connection to the Arduino board
+        """
+        self.ser = serial.Serial(serial_port, baud_rate)
+        self.ser.timeout = read_timeout # Timeout for readline()
+
+    def moveTo(self, motor_number, value):
+        """
+        Writes the digital_value on pin_number
+        Internally sends b'WD{pin_number}:{digital_value}' over the serial
+        connection
+        """
+        #command = (''.join(('MX ', str(motor_number), ' ',
+        #command = (''.join(('MX ', ' ',
+        #    str(value)))).encode()
+        #Unicode strings must be encoded (e.g. 'hello'.encode('utf-8').
+        command = f'MX {value}\n'.encode('utf-8')
+        print(command)
+        #self.ser.write(command)
+        #line = ser.readline()   # read a '\n' terminated line
+        #print(line)
+
+    def close(self):
+        """
+        To ensure we are properly closing our connection to the
+        Arduino device.
+        """
+        self.ser.close()
+        print('Connection to RP2040 closed')
+
---
+++ python/WriteCsv.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+A program to read data from the High-Precision-AD-HAT
+and save to a CSV file
+"""
+
+import time
+import sys
+import numpy as np
+
+from MiniNirAdc import MiniNirAdc
+
+NUM_SAMPLES = 6000
+NUM_CHANNELS = 6
+
+timestr = time.strftime("%Y%m%d-%H%M%S")
+
+if len(sys.argv) > 1:
+    filename = str(sys.argv[1])
+else:
+    filename = 'dados_test1/BI_' + timestr + '.csv'
+
+try:
+    adc = MiniNirAdc()
+    if (adc.open() == -1):
+        print('Open Error')
+        exit()
+
+    adcVals = adc.read(num_samples=60, num_channels=6)
+    adc.close()
+    
+    print('Acquisition Done. Saving Data to ' + filename)
+    np.savetxt(filename, adcVals, fmt='%.5f', delimiter=',')
+
+except IOError as e:
+    print(e)
+   
+except KeyboardInterrupt:
+    print("ctrl + c:")
+    print("Program end")
+    exit()
+
+# vim: sta:et:sw=4:ts=4:sts=4
+# -*- coding:utf-8 -*-
